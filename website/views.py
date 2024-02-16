5aca17aa95b7e6a72cdf7a775fc049e70ff151c8
Matt Rogue <mahthiar@gmail.com>
2024-02-16 18:56:57 +0000

    Redesigned navigation

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+from .models import StaffType
 
 views = Blueprint('views', __name__)
 
@@ -7,7 +8,10 @@ views = Blueprint('views', __name__)
 @views.route('/home/')
 @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    return render_template('home.html',
+        user=current_user,
+        StaffType=StaffType
+    )
 
 
 
